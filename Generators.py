e9a27bb868a2c10a6d6583a244115b1bed001870
KalebYigezu <43226925+KalebYigezu@users.noreply.github.com>
2024-09-22 17:24:28 -0700

    Update Generators.py

--- Generators.py
+++ Generators.py
@@ -1,1 +1,23 @@
 
+def get_names(fname):
+    with open(fname, 'r') as file:
+        lines = []
+        for line in file:
+            lines.append(line)
+        return lines
+
+names = get_names("Names.txt")
+print(names)
+
+-------------------------- instead -----------------------
+
+
+def get_names(fname):
+    with open(fname, 'r') as file:
+        for line in file:
+            yield line
+
+names = get_names("Names.txt")
+
+print(next(names))
+
