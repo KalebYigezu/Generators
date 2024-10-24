d9e21f6df5b1a8b8b610ff5ff24404e4f3215fb9
KalebYigezu <43226925+KalebYigezu@users.noreply.github.com>
2024-10-23 20:17:11 -0700

    Update Generators.py

--- Generators.py
+++ Generators.py
@@ -3,10 +3,13 @@ def get_names(fname):
     with open(fname, 'r') as file:
         lines = []
         for line in file:
-            lines.append(line)
+            lines.append(line.rstrip("\n")) # rstrip is because we don't want \n to be read from the file
         return lines
 
+
 names = get_names("Names.txt")
+
+
 print(names)
 
 -------------------------- instead -----------------------
